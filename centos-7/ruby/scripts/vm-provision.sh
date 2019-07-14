edf86f886192945c3accde7066b82c5c01bd58b5
Marco Marassi <thtg88@users.noreply.github.com>
2019-07-14 16:33:11 +0100

    Removed MariaDB from provisioning as not needed for the time-being

--- centos-7/ruby/scripts/vm-provision.sh
+++ centos-7/ruby/scripts/vm-provision.sh
@@ -5,7 +5,7 @@ yum update -y
 # We install nodejs for Ruby to have a JS dev environment
 yum install -y yum-utils bind-utils patch vim nano wget epel-release net-tools zip unzip git
 # Install Apache (httpd), MariaDB, and its extensions from the local repo
-yum install -y httpd httpd-devel mariadb-server epel-release mod_ssl nodejs
+yum install -y httpd httpd-devel epel-release mod_ssl nodejs
 # Update CentOS packages
 yum -y update
 mkdir -p /var/www/certificates
@@ -14,27 +14,6 @@ mkdir -p /var/www/vhosts
 sed -i 's"^SELINUX=.*$"SELINUX=disabled"' /etc/selinux/config
 # set system timezone
 timedatectl set-timezone Europe/London
-# Set MySQL's auto_increment_increment and auto_increment_offset to 2
-# to emulate the live server's behaviour
-sed -i 's"^\[mysqld\].*$"\[mysqld\]\nauto_increment_increment=2\nauto_increment_offset=2"' /etc/my.cnf
-# enable and start Apache and MariaDB services
-systemctl enable mariadb.service
-systemctl start mariadb.service
-# set MySQL root password
-sudo /usr/bin/mysqladmin -u root password 'root'
-# allow MySQL remote access (required to access from our private network host. Note that this is completely insecure if used in any other way)
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'root' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-# allow MySQL remote access to somdes (required to access from our private network host. Note that this is completely insecure if used in any other way)
-mysql -u root -proot -e "GRANT ALL PRIVILEGES ON *.* TO 'user'@'%' IDENTIFIED BY 'usersecret' WITH GRANT OPTION;"
-# drop the MySQL anonymous users
-mysql -u root -proot -e "DROP USER ''@'localhost';"
-mysql -u root -proot -e "DROP USER ''@'$(hostname)';"
-# drop the MySQL demo database
-mysql -u root -proot -e "DROP DATABASE test;"
-# reload MySQL privileges table
-mysql -u root -proot -e "FLUSH PRIVILEGES;"
-# enable and start MariaDB service
-systemctl restart mariadb.service
 # Set ServerName vagrant.localhost
 sed -i 's"^#ServerName .*$"ServerName vagrant.localhost"' /etc/httpd/conf/httpd.conf
 # configure Apache
