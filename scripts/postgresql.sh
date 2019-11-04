d25054c6ef63d460bfc400cbe500766ab1dc9e66
Marco Marassi <thtg88@users.noreply.github.com>
2019-11-04 16:47:42 +0000

    Added postgres-devel package to PostgreSQL script setup

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -4,7 +4,7 @@
 
 # As of this writing, the CentOS 7 repositories ship
 # with PostgreSQL version 9.2.15
-yum install -y postgresql-server postgresql-contrib
+yum install -y postgresql-server postgresql-contrib postgresql-devel
 
 # Initialize your Postgres database and start PostgreSQL
 postgresql-setup initdb
