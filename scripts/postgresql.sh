aee6f721e636bd5c3c00baffb6e905ffa8052b14
Marco Marassi <thtg88@users.noreply.github.com>
2019-11-06 09:13:59 +0000

    Added remote connection config to PostgreSQL setup

--- scripts/postgresql.sh
+++ scripts/postgresql.sh
@@ -15,3 +15,14 @@ systemctl enable postgresql
 
 # Create vagrant postgres superuser
 sudo -u postgres createuser -s vagrant
+
+# Add host machine IP address to white-list
+cat >> /var/lib/pgsql/data/pg_hba.conf <<EOF
+host all all 10.0.2.2/24 trust
+EOF
+
+# Listen connections from all addresses
+sed -i 's"^#listen_addresses = ''localhost''.*$"listen_addresses = ''*''"' /var/lib/pgsql/data/postgresql.conf
+
+# Restart
+systemctl restart postgresql
