f5986cf3d7f03148c11d50dc9953f2126a1595f0
Marco Marassi <thtg88@users.noreply.github.com>
2019-11-08 09:55:29 +0000

    Added comment about SQL Server setup

--- scripts/sql-server.sh
+++ scripts/sql-server.sh
@@ -4,4 +4,6 @@
 curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/7/mssql-server-2017.repo
 yum install -y mssql-server
 
+# Remember to run the following:
+# sudo /opt/mssql/bin/mssql-conf setup
 # Password1234
