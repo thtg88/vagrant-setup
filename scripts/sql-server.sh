fd58bc54463ce823f03666b64d8bd42fda9c07a3
Marco Marassi <thtg88@users.noreply.github.com>
2019-11-19 13:29:26 +0000

    Updated SQL Server setup script

--- scripts/sql-server.sh
+++ scripts/sql-server.sh
@@ -1,9 +1,112 @@
 #!/usr/bin/env bash
 
-# Install and configure SQL Server
+# Install and configure Microsoft SQL Server
+
+# Source:
+# https://docs.microsoft.com/en-us/sql/linux/quickstart-install-connect-red-hat?view=sql-server-ver15
+# https://docs.microsoft.com/en-us/sql/linux/sample-unattended-install-redhat?view=sql-server-ver15
+
+# Use the following variables to control your install:
+
+# Password for the SA user (required)
+MSSQL_SA_PASSWORD='Password1234'
+
+# Product ID of the version of SQL server you're installing
+# Must be evaluation, developer, express, web, standard, enterprise, or your 25 digit product key
+# Defaults to developer
+MSSQL_PID='developer'
+
+# Install SQL Server Agent (recommended)
+# SQL_ENABLE_AGENT='y'
+
+# Install SQL Server Full Text Search (optional)
+# SQL_INSTALL_FULLTEXT='y'
+
+# Create an additional user with sysadmin privileges (optional)
+# SQL_INSTALL_USER='<Username>'
+# SQL_INSTALL_USER_PASSWORD='<YourStrong!Passw0rd>'
+
+if [ -z $MSSQL_SA_PASSWORD ]
+then
+    echo Environment variable MSSQL_SA_PASSWORD must be set for unattended install
+    exit 1
+fi
+
+echo Adding Microsoft repositories...
 curl -o /etc/yum.repos.d/mssql-server.repo https://packages.microsoft.com/config/rhel/7/mssql-server-2017.repo
+curl -o /etc/yum.repos.d/msprod.repo https://packages.microsoft.com/config/rhel/7/prod.repo
+
+echo Installing SQL Server...
 yum install -y mssql-server
 
-# Remember to run the following:
-# sudo /opt/mssql/bin/mssql-conf setup
-# Password1234
+echo Running mssql-conf setup...
+MSSQL_SA_PASSWORD=$MSSQL_SA_PASSWORD MSSQL_PID=$MSSQL_PID /opt/mssql/bin/mssql-conf -n setup accept-eula
+
+echo Installing mssql-tools and unixODBC developer...
+ACCEPT_EULA=Y yum install -y mssql-tools unixODBC-devel
+
+# Add SQL Server tools to the path by default:
+echo Adding SQL Server tools to your path...
+echo PATH="$PATH:/opt/mssql-tools/bin" >> ~/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+echo PATH="$PATH:/opt/mssql-tools/bin" >> /home/vagrant/.bash_profile
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> /home/vagrant/.bashrc
+source ~/.bashrc
+
+# Optional Enable SQL Server Agent :
+if [ ! -z $SQL_ENABLE_AGENT ]
+then
+    echo Enable SQL Server Agent...
+    /opt/mssql/bin/mssql-conf set sqlagent.enabled true
+    systemctl restart mssql-server
+fi
+
+# Optional SQL Server Full Text Search installation:
+if [ ! -z $SQL_INSTALL_FULLTEXT ]
+then
+    echo Installing SQL Server Full-Text Search...
+    yum install -y mssql-server-fts
+fi
+
+# Configure firewall to allow TCP port 1433:
+# echo Configuring firewall to allow traffic on port 1433...
+# sudo firewall-cmd --zone=public --add-port=1433/tcp --permanent
+# sudo firewall-cmd --reload
+
+# Example of setting post-installation configuration options
+# Set trace flags 1204 and 1222 for deadlock tracing:
+#echo Setting trace flags...
+#sudo /opt/mssql/bin/mssql-conf traceflag 1204 1222 on
+
+# Restart SQL Server after making configuration changes:
+echo Restarting SQL Server...
+systemctl restart mssql-server
+
+# Connect to server and get the version:
+counter=1
+errstatus=1
+while [ $counter -le 5 ] && [ $errstatus = 1 ]
+do
+    echo Waiting for SQL Server to start...
+    sleep 5s
+    /opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD -Q "SELECT @@VERSION" 2>/dev/null
+    errstatus=$?
+    ((counter++))
+done
+
+# Display error if connection failed:
+if [ $errstatus = 1 ]
+then
+    echo Cannot connect to SQL Server, installation aborted
+    exit $errstatus
+fi
+
+# Optional new user creation:
+if [ ! -z $SQL_INSTALL_USER ] && [ ! -z $SQL_INSTALL_USER_PASSWORD ]
+then
+echo Creating user $SQL_INSTALL_USER
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P $MSSQL_SA_PASSWORD \
+    -Q "CREATE LOGIN [$SQL_INSTALL_USER] WITH PASSWORD=N'$SQL_INSTALL_USER_PASSWORD', DEFAULT_DATABASE=[master], CHECK_EXPIRATION=ON, CHECK_POLICY=ON; ALTER SERVER ROLE [sysadmin] ADD MEMBER [$SQL_INSTALL_USER]"
+fi
+
+echo Done!
